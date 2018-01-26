39cbba7bc31d2cbce89c699aabc3d53fe38cb214
Mike Long <mike@praqma.com>
2018-01-26 22:45:20 +0100

    Reformat server.go to follow conventions

--- server.go
+++ server.go
@@ -1,14 +1,16 @@
 package main
+
 import (
-  "net/http"
+	"net/http"
 )
+
 func ping(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("pong"))
+	w.Write([]byte("pong"))
 }
 func main() {
-  http.Handle("/", http.FileServer(http.Dir("./site")))
-  http.HandleFunc("/ping", ping)
-  if err := http.ListenAndServe(":80", nil); err != nil {
-    panic(err)
-  }
+	http.Handle("/", http.FileServer(http.Dir("./site")))
+	http.HandleFunc("/ping", ping)
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		panic(err)
+	}
 }
